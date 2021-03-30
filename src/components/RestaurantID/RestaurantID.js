a61f872d44e36a5c8b8803c3610cbd07a514254d
jjcrab <lily_lijj@hotmail.com>
2021-03-30 16:31:24 -0600

    update basic setup

--- src/components/RestaurantID/RestaurantID.js
+++ src/components/RestaurantID/RestaurantID.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import axios from 'axios';
 import APIurl from '../../config';
+import './RestaurantID.css';
 
 const RestaurantID = ({ match }) => {
 	let history = useHistory();
@@ -75,7 +76,7 @@ const RestaurantID = ({ match }) => {
 			</p>
 			<p>Phone: {restaurant.Phone}</p>
 			<p>Address: {restaurant.address}</p>
-			<div>
+			<div className='restPics'>
 				<img src={restaurant.restaurantImg} alt='restaurant picture' />
 				<img src={restaurant.dishImgOne} alt='dish picture one' />
 				<img src={restaurant.dishImgOne} alt='dish picture two' />
