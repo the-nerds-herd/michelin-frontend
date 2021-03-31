85c60d6c026fec2a0cf3b3657b50f232328a0362
jjcrab <lily_lijj@hotmail.com>
2021-03-31 10:12:12 -0600

    update background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 // import './App.css';
 import { Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Home from './components/Home'
+import Home from './components/Home/Home';
 import Restaurants from './components/Restaurants/Restaurants';
 import RestaurantID from './components/RestaurantID/RestaurantID';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<Home />
-
 				<h1>
 					<img
 						src='https://www.nicepng.com/png/full/81-812831_michelin-star-jpg.png'
@@ -25,6 +22,7 @@ function App() {
 				<Nav />
 			</header>
 			<main>
+				<Route path='/' exact component={Home} />
 				<Route path='/restaurants' exact component={Restaurants} />
 				<Route path='/restaurants/:id' component={RestaurantID} />
 			</main>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,12 @@
-import { Container } from '@material-ui/core';
 import React from 'react';
-import {Nav, Navbar, Col, Image} from 'react-bootstrap'
+import { Nav, Navbar, Col, Image } from 'react-bootstrap';
 
 import { WiStars } from 'react-icons/wi';
-
+import './Home.css';
 
 const Home = () => {
-    return (
-        <div>
+	return (
+		<div className='home'>
 			<Navbar className='navbar' variant='dark'>
 				<Navbar.Brand href='#home'>
 					<WiStars className='star' />
@@ -24,14 +23,16 @@ const Home = () => {
 					</Nav.Link>
 				</Nav>
 			</Navbar>
-              <Image className='michelin-pic' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKK6ls_q00-jw2RHFKt--Eoun1zdiHU2eL0w&usqp=CAU" roundedCircle fluid />
+			<Image
+				className='michelin-pic'
+				src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKK6ls_q00-jw2RHFKt--Eoun1zdiHU2eL0w&usqp=CAU'
+				roundedCircle
+				fluid
+			/>
 
-      {/* <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFAc4PXk8yxyFkfF2saHjDNhQexJ4EvJ4yHW5UPIEHw-7ZemNAaaAQjLqFKv2lJ1_exDw&usqp=CAU" roundedCircle /> */}
- 
-            
-            </div>
-
-		);
+			{/* <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFAc4PXk8yxyFkfF2saHjDNhQexJ4EvJ4yHW5UPIEHw-7ZemNAaaAQjLqFKv2lJ1_exDw&usqp=CAU" roundedCircle /> */}
+		</div>
+	);
 };
 
 export default Home;
