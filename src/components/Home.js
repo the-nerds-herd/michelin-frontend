148c8ede13278a4fa2f8b4d9a6e3dd97370d6320
andrew childre <andrewchildre@gmail.com>
2021-03-30 21:16:49 -0600

    home-pg-2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
-import {Nav, Navbar, FormControl, Form, Button} from 'react-bootstrap'
+import {Nav, Navbar, Col, Image} from 'react-bootstrap'
 
 import { WiStars } from 'react-icons/wi';
 
 
 const Home = () => {
     return (
+        <div>
 			<Navbar className='navbar' variant='dark'>
 				<Navbar.Brand href='#home'>
 					<WiStars className='star' />
@@ -22,6 +24,13 @@ const Home = () => {
 					</Nav.Link>
 				</Nav>
 			</Navbar>
+              <Image className='michelin-pic' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKK6ls_q00-jw2RHFKt--Eoun1zdiHU2eL0w&usqp=CAU" roundedCircle fluid />
+
+      {/* <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFAc4PXk8yxyFkfF2saHjDNhQexJ4EvJ4yHW5UPIEHw-7ZemNAaaAQjLqFKv2lJ1_exDw&usqp=CAU" roundedCircle /> */}
+ 
+            
+            </div>
+
 		);
 };
 
