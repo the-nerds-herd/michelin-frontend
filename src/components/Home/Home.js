d33a35904a77d20ca99703577797e1c5d634cf7e
andrew childre <andrewchildre@gmail.com>
2021-03-31 17:16:55 -0600

    nav-btm

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Nav, Navbar, Col, Image } from 'react-bootstrap';
-
+import { Nav, Navbar, Container, Image } from 'react-bootstrap';
 import { WiStars } from 'react-icons/wi';
 import './Home.css';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<Navbar className='navbar' variant='dark'>
+			<Container className='center' fluid>
+			<Navbar className='navbar' fixed="bottom" variant='dark'>
 				<Navbar.Brand href='#home'>
-					<WiStars className='star' />
+					{/* <WiStars className='star' /> */}
 				</Navbar.Brand>
 				<Nav id='mr-auto'>
 					<Nav.Link href='#home'>
@@ -23,13 +23,17 @@ const Home = () => {
 					</Nav.Link>
 				</Nav>
 			</Navbar>
+			
+
+			<h1 class='title'>Michelin Star Finder</h1>
+
 			<Image
 				className='michelin-pic'
-				src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKK6ls_q00-jw2RHFKt--Eoun1zdiHU2eL0w&usqp=CAU'
+				src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNHM2mSSKXq1K7N3p7cqXldDh3kGPda-nY-FdRA1kmlA6d9G12mD3AV0_-YXvrS-8al_0&usqp=CAU'
 				roundedCircle
-				fluid
+				
 			/>
-
+				</Container>
 			{/* <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFAc4PXk8yxyFkfF2saHjDNhQexJ4EvJ4yHW5UPIEHw-7ZemNAaaAQjLqFKv2lJ1_exDw&usqp=CAU" roundedCircle /> */}
 		</div>
 	);
