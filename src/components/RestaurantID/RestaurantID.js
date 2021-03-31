cce7ff1bbb7a2d4099aa1a59a29c566bb2344e7f
jjcrab <lily_lijj@hotmail.com>
2021-03-31 09:28:08 -0600

    update restaurant component

--- src/components/RestaurantID/RestaurantID.js
+++ src/components/RestaurantID/RestaurantID.js
@@ -20,66 +20,78 @@ const RestaurantID = ({ match }) => {
 	}
 
 	return (
-		<div>
-			<h1>{restaurant.name}</h1>
-			<div>
-				{restaurant.starNumber === 3 && (
-					<div>
-						<img
-							src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
-							alt='stars'
-							width='3%'
-						/>
-						<img
-							src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
-							alt='stars'
-							width='3%'
-						/>
-						<img
-							src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
-							alt='stars'
-							width='3%'
-						/>
-					</div>
-				)}
+		<div className='restContainer'>
+			<div className='restInform'>
+				<h1>{restaurant.name}</h1>
+				<div>
+					{restaurant.starNumber === 3 && (
+						<div>
+							<img
+								src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
+								alt='stars'
+								width='3%'
+							/>
+							<img
+								src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
+								alt='stars'
+								width='3%'
+							/>
+							<img
+								src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
+								alt='stars'
+								width='3%'
+							/>
+						</div>
+					)}
 
-				{restaurant.starNumber === 2 && (
-					<div>
-						<img
-							src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
-							alt='stars'
-							width='3%'
-						/>
-						<img
-							src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
-							alt='stars'
-							width='3%'
-						/>
-					</div>
-				)}
+					{restaurant.starNumber === 2 && (
+						<div>
+							<img
+								src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
+								alt='stars'
+								width='3%'
+							/>
+							<img
+								src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
+								alt='stars'
+								width='3%'
+							/>
+						</div>
+					)}
 
-				{restaurant.starNumber === 1 && (
-					<img
-						src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
-						alt='stars'
-						width='3%'
-					/>
-				)}
+					{restaurant.starNumber === 1 && (
+						<img
+							src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/MichelinStar.svg/1200px-MichelinStar.svg.png'
+							alt='stars'
+							width='3%'
+						/>
+					)}
+				</div>
+				<div className='restText'>
+					{/* <p>Start(s) Number: {restaurant.starNumber}</p> */}
+					<p>Introcution (from Michelin Guide)</p>
+					<p>{restaurant.introduction}</p>
+					<a href={restaurant.website}>{restaurant.website}</a>
+					<p>Opening Hours: {restaurant.openingHours}</p>
+					<p>
+						<a href={restaurant.menue}>Menue</a>
+					</p>
+					<p>Phone: {restaurant.Phone}</p>
+					<p>Address: {restaurant.address}</p>
+					<div className='restPics'>
+						<img src={restaurant.restaurantImg} alt='restaurant picture' />
+						<img src={restaurant.dishImgOne} alt='dish picture two' />
+					</div>
+				</div>
 			</div>
-			{/* <p>Start(s) Number: {restaurant.starNumber}</p> */}
-			<p>Introcution (from Michelin Guide)</p>
-			<p>{restaurant.introduction}</p>
-			<a href={restaurant.website}>{restaurant.website}</a>
-			<p>Opening Hours: {restaurant.openingHours}</p>
-			<p>
-				<a href={restaurant.menue}>Menue</a>
-			</p>
-			<p>Phone: {restaurant.Phone}</p>
-			<p>Address: {restaurant.address}</p>
-			<div className='restPics'>
-				<img src={restaurant.restaurantImg} alt='restaurant picture' />
-				<img src={restaurant.dishImgOne} alt='dish picture one' />
-				<img src={restaurant.dishImgOne} alt='dish picture two' />
+			<div className='reviews'>
+				<div className='reviewsImg'>
+					<img src={restaurant.dishImgTwo} alt='dish picture one' />
+				</div>
+				<div className='reviewsContainer'>
+					<h3>Reviews</h3>
+					<div className='reviewform'></div>
+				</div>
 			</div>
 		</div>
 	);
