53ce21e2a5fe7a3d5023cc6a3c9fa87eb0a1a9eb
jjcrab <lily_lijj@hotmail.com>
2021-04-05 07:47:31 -0600

    delete expiration

--- src/components/User/UserLogin.js
+++ src/components/User/UserLogin.js
@@ -20,7 +20,6 @@ function LoginPage() {
 			.post(`${APIurl}/users/signin`, formState)
 			.then(({ data }) => {
 				localStorage.setItem('token', data.token);
-				localStorage.setItem('expiration', Date.now() + 3600);
 			})
 			.then(() => {
 				setUser(formState.name);
